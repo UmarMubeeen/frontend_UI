581faeb09da046e82a89ac874ab01f2ed5797d95
umarMubeeen <umarmubinkhan@gmail.com>
2024-09-12 22:40:35 +0500

    card linked to other pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    colors:{
-      mango:  'hsl(47, 88%, 63%)',
-      white: 'hsl(0, 0, 100%)',
-      Gray : 'hsl(0, 0%, 42%)'
-    },
+   
     extend: {},
   },
   plugins: [],

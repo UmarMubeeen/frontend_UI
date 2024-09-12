ec453d88fb479995ac64b1a140afeeb0598f6f4b
umarMubeeen <umarmubinkhan@gmail.com>
2024-09-12 22:24:17 +0500

    card created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
+    colors:{
+      mango:  'hsl(47, 88%, 63%)',
+      white: 'hsl(0, 0, 100%)',
+      Gray : 'hsl(0, 0%, 42%)'
+    },
     extend: {},
   },
   plugins: [],
